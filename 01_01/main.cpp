f124987463fd63baae001a0eee2409915c2cf545
Vl-VSV <vvs_2015@icloud.com>
2022-09-03 16:59:34 +0300

    Add 01_01

---
+++ 01_01/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  01_01
+//
+//  Created by Vlad V on 03.09.2022.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double n1, n2;
+    
+    cout << "Введите 1-е число" << endl;
+    cin >> n1;
+    
+    cout << "Введите 2-е число" << endl;
+    cin >> n2;
+    
+    
+    cout << "Сумма " << n1+n2 << endl;
+    cout << "Разность " << n1-n2<< endl;
+    cout << "Произведение " << n1*n2 << endl;
+
+    n2 != 0 ? cout << "Целая часть деления " << int(n1/n2) << endl: cout << "n2 == 0, division by zero";
+    
+}
--- ProceduralProgramming/main.cpp
+++ ProceduralProgramming/main.cpp
@@ -9,6 +9,6 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    cout << "Hello world";
+    cout << "Hello world" << endl;
     return 0;
 }
