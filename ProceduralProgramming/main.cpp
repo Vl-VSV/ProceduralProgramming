c5e762adcb44f6eb6c339725e734dcce34ed8d58
Vl-VSV <vvs_2015@icloud.com>
2022-09-03 16:52:43 +0300

    Initial Commit

---
+++ ProceduralProgramming/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ProceduralProgramming
+//
+//  Created by Vlad V on 03.09.2022.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cout << "Hello world";
+    return 0;
+}
