d0ffb0d0f0d2bda679e85f079cdbde17eba59907
Vlad V <72020591+Vl-VSV@users.noreply.github.com>
2022-09-09 10:09:16 +0300

    update 03_05

--- 03_05/main.cpp
+++ 03_05/main.cpp
@@ -5,6 +5,7 @@
 //  Created by Vlad V on 04.09.2022.
 //
 
+/*
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -17,4 +18,24 @@ int main() {
     cout << "Enter string form 30 simvols" << endl; cin >> s; cout << endl;
     sort(s.begin(), s.end());
     cout << s << endl;
+}
+*/
+
+
+#include <iostream>
+
+using namespace std;
+//dasfsadhfkjlrheuladjkfhlfhrefk
+
+int main() {
+    string s;
+    cout << "Enter string form 30 simvols" << endl; cin >> s; cout << endl;
+    for (int i = 0; i < 30; i++){
+      for (int j = 0; j < 30 - i - 1; j++){
+        if (s[j] > s[j + 1]){
+          swap(s[j], s[j+1]);
+          }
+      }
+    }
+cout << "Result " << s;
 }
