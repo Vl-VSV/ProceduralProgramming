e4aa7b505e12f8380b6cc82d8e2bbc1aef2e3729
Vlad V <72020591+Vl-VSV@users.noreply.github.com>
2022-09-09 13:05:44 +0300

    update 04_04

--- 04_04/main.cpp
+++ 04_04/main.cpp
@@ -12,17 +12,42 @@ using namespace std;
 int main () {
     string s1 = "* * * * * * * * ";
     string s2 = "";
+    string s3 = "* * * * * * * * ";
+    string s4 = "";
+  
+    bool flag = false;
     for (int i = 0; i < 44; i++)
         s1 += "|";
+    for (int i = 0; i < 44; i++)
+        s3 += "–";
     
     for (int i = 0; i < 60; i++)
         s2 += "|";
+    for (int i = 0; i < 60; i++)
+        s4 += "–";
     
-    for (int i = 0; i < 7; i++)
+    for (int i = 0; i < 7; i++){
+      if (flag){
         cout << s1 << endl;
+        flag = false;
+      }
+      else{
+        cout << s3 << endl;
+        flag = true;
+      }
+  }
+        
     
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++){
+      if (flag){
         cout << s2 << endl;
+        flag = false;
+      }
+      else{
+        cout << s4 << endl;
+        flag = true;
+      }
+  }
     
     return 0;
 }
