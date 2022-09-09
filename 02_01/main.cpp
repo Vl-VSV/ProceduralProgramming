2648928aaee5a9b821b8214cadb55fde872bfb14
Vlad V <72020591+Vl-VSV@users.noreply.github.com>
2022-09-09 09:27:50 +0300

    update 02_01

--- 02_01/main.cpp
+++ 02_01/main.cpp
@@ -15,6 +15,9 @@ int main() {
     double R, r, h, l;
     cout << "Enter R (top), r (bottom) and h" << endl;
     cin >> R >> r >> h;
+    if (R <= 0 || r <= 0 || h <= 0){
+        cout << "Error" << endl;
+    }
     l = sqrt(h*h + (r-R)*(r-R));
     cout << "V = " << M_PI * h * (R * R + R * r + r * r) / 3.0 << endl;
     cout << "S = " << M_PI *(R*R + (R + r)*l + r*r) << endl;
